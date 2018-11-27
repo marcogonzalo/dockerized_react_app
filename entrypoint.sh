593fd66cf80782f0a0f8070198ae53e027223b5c
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2018-11-27 22:46:08 +0100

    Dockerfile y docker-compose para un proyecto vacio de React

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+yarn install --pure-lockfile
+yarn add react react-dom
+yarn add @babel/core @babel/preset-env @babel/preset-react
+yarn add webpack webpack-cli babel-loader
+yarn add --dev webpack-dev-server
+
+exec "$@"
